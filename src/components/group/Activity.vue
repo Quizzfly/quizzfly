43b9e8ed3b1d6eb3fc3b139b8b1bd10bea9be976
trungdong11 <trungdong0811@gmail.com>
2024-11-25 16:08:48 +0700

    [QUIZ-144][QUIZ-149] fix: responsive & fix modal create post

--- src/components/group/Activity.vue
+++ src/components/group/Activity.vue
@@ -8,7 +8,6 @@ import { useAuthStore } from '@/stores/auth'
 import { usePostStore } from '@/stores/group/post'
 import { formatDateTime } from '@/utils/time'
 import { useConfirmDialog } from '@/stores/modal'
-import sanitizeHtml from 'sanitize-html'
 
 const confirmDialog = useConfirmDialog()
 const authStore = useAuthStore()
@@ -122,7 +121,7 @@ const handleDeletePost = async (id: string) => {
           <div class="h-px w-full bg-slate-200"></div>
           <div class="px-6 py-4">
             <div class="flex flex-col gap-2">
-              <p v-html="sanitizeHtml(item?.content)"></p>
+              <p v-html="$sanitize(item?.content)"></p>
               <img
                 class="h-64 w-full rounded-xl object-cover"
                 src="@/assets/img/bg-image-1.jpg"
